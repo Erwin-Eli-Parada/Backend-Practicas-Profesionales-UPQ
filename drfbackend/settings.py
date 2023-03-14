eb0794071bcfaa02c16e52f342646c96c944a02e
Eli <perwin119@gmail.com>
2023-03-13 22:47:30 -0600

    actualizacion version de django

--- drfbackend/settings.py
+++ drfbackend/settings.py
@@ -84,10 +84,12 @@ WSGI_APPLICATION = 'drfbackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-       default='sqlite3:///db.sqlite3',        
-       conn_max_age=600 
-    )
+    # 'default':{
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite',conn_max_age=600)
 }
 
 
@@ -130,7 +132,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
