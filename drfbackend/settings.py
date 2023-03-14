894d00ff46f5cf5b5bce590f7d6958c2db9a902e
Eli <perwin119@gmail.com>
2023-03-14 12:44:28 -0600

    actualizacion para produccion

--- drfbackend/settings.py
+++ drfbackend/settings.py
@@ -86,15 +86,15 @@ WSGI_APPLICATION = 'drfbackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default':{
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 
-    # 'default': dj_database_url.config(
-    #     default='sqlite///db.sqlite3',
-    #     conn_max_age=600
-    # )
+    'default': dj_database_url.config(
+        default='sqlite///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
