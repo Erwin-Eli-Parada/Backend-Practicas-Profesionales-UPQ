4941322348cca5f69f5abd9aa3126879bd651bef
Eli <perwin119@gmail.com>
2023-05-22 13:48:55 -0600

    ruta para grafica uno

--- datos/api/urls.py
+++ datos/api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views.encuestaViews import encuesta_detail_view
+from .views.graficoTipoView import graficoTipoView
 
 urlpatterns = [
     path('datos/',include('datos.api.routers')),
     path('datos/encuestaAlumno/<str:pk>/', encuesta_detail_view, name='encuesta_api'),
+    path('grafico/tipo/', graficoTipoView, name='grafico_tipo'),
 ]
