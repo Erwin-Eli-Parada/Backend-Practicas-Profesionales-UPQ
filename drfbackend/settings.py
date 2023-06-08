7df79c4ad603f9b97733c50b8d45bf67948d0059
Eli <perwin119@gmail.com>
2023-06-07 18:00:03 -0600

    deploy

--- drfbackend/settings.py
+++ drfbackend/settings.py
@@ -121,7 +121,8 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'users.Usuario'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://resplendent-crostata-5bc63a.netlify.app"
 ]
 
 CORS_ORIGIN_WHITELIST = [
